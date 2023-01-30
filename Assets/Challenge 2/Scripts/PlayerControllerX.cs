da1603d8f45589f3ae0a014337eea9327293b9c9
Francesco Ponzo <mr.franceso.ponzo@gmail.com>
2023-01-30 12:30:53 +0000

    initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float timeStart = 0.0f;
+    private float timeRepeat = 1.0f;
+    private bool acceptInput = false;
+
+    private void Start()
+    {
+        InvokeRepeating("SetAcceptInput", 0.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && acceptInput)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            acceptInput = false;
+        }
+    }
+
+    private void SetAcceptInput()
+    {
+        acceptInput = true;
+    }
+}
